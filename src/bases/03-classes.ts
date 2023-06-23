50229ff1b46e9d5efd2e30888897f4f0c7e8f5ed
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-06-22 20:37:09 -0400

    colocar tipo de datos a respuestas http

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,10 +1,12 @@
+import axios from 'axios';
+import { Move, PokeapiReponse } from '../interfaces/pokeapi-response.interface';
 
 export class Pokemon {
 
     get imageUrl(): string {
         return `https://pokemon.com/${ this.id }.jpg`;
     }
-
+  
     constructor(
         public readonly id: number, 
         public name: string,
@@ -18,9 +20,17 @@ export class Pokemon {
 
     speak() {
         console.log(`${ this.name }, ${ this.name }`);
-    }   
+    }
+
+    async getMoves(): Promise<Move[]> {
+        const { data } = await axios.get<PokeapiReponse>('https://pokeapi.co/api/v2/pokemon/4');
+        console.log( data.moves );
+        
+        return data.moves;
+    }
 
 }
 
 export const charmander = new Pokemon( 4, 'Charmander' );
 
+charmander.getMoves();
