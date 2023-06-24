ae3899354d6ab18f78505c1a9a07c31e512a461b
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-06-24 10:26:53 -0400

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-//import { name, age } from './bases/01-types';
 // import { bulbasaur, pokemons } from './bases/02-objects';
-import { charmander } from './bases/03-classes';
+// import { name, age } from './bases/01-types';
+// import { charmander } from './bases/03-classes';
+// import { charmander } from './bases/04-injection';
+ import { charmander } from './bases/05-decorators';
 
 import './style.css';
 
